ad478701bc83d1fb17efc5bfc710b336ccea0784
drewgood24 <99758212+drewgood24@users.noreply.github.com>
2022-03-01 13:56:05 -0600

    arrowkeys

---
+++ Snake/testpack/test1.java
@@ -0,0 +1,7 @@
+//testing Ellery Sabado
+package Snake.testpack;
+    public class test1{
+        public static void main(String args[]){
+            System.out.println("Solid Bruh Moment");
+        }
+    }
